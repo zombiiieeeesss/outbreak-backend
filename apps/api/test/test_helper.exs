fcfad93cc57b922a75ccec8654cb18bf86a15d66
Michael Smykowski <smycal@gmail.com>
2017-07-29 18:56:08 -0700

    update test helper json_response to handle nested maps

--- apps/api/test/test_helper.exs
+++ apps/api/test/test_helper.exs
@@ -4,6 +4,11 @@ defmodule API.Web.Controller.TestHelper do
   def json_response(res) do
     res.resp_body
     |> Poison.decode!
-    |> Map.new(fn({k,v}) -> {String.to_atom(k), v} end)
+    |> Map.new(&string_to_atom_keys/1)
   end
+
+  defp string_to_atom_keys({k, v}) when is_map(v) do
+    {String.to_atom(k), Map.new(v, &string_to_atom_keys/1)}
+  end
+  defp string_to_atom_keys({k, v}), do: {String.to_atom(k), v}
 end
