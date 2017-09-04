f0e033adfa38b2c28c427130a7062e15114eb051
Michael Smykowski <smycal@gmail.com>
2017-09-03 21:13:19 -0700

    add seeds function

--- apps/db/priv/repo/seeds.exs
+++ apps/db/priv/repo/seeds.exs
@@ -1,8 +1,19 @@
 defmodule DB.Seed do
-  def create_users do
+  def seed do
+    user = create_user()
+
+    game = create_game(user.id)
+    create_player(user.id, game.id)
+
     0..number_of_inserts()
-    |> Task.async_stream(fn(_) -> create_user() end, [max_concurrency: 10])
+    |> Task.async_stream(fn(_) ->
+        fr = create_friend_request(user.id)
+        create_player(fr.requested_user_id, game.id)
+      end, [max_concurrency: 10])
     |> Stream.run
+
+    # credo:disable-for-next-line
+    IO.inspect "Data Seeded for-- username: #{user.username}/id: #{user.id}"
   end
 
   defp create_user do
@@ -14,6 +25,48 @@ defmodule DB.Seed do
 
     # credo:disable-for-next-line
     IO.inspect "Created User-- #{user.username}/#{user.id}"
+
+    user
+  end
+
+  defp create_friend_request(requesting_user_id \\ nil, requested_user_id \\ nil) do
+    {:ok, fr} = DB.FriendRequest.create(
+      requesting_user_id || create_user().id,
+      requested_user_id || create_user().id,
+      FakerElixir.Helper.pick(~w(pending accepted))
+      )
+
+    # credo:disable-for-next-line
+    IO.inspect "Created Friend Request-- #{fr.id}"
+
+    fr
+  end
+
+  defp create_player(user_id \\ nil, game_id \\ nil) do
+    {:ok, player} = DB.Player.create(%{
+      status: FakerElixir.Helper.pick(~w(user-pending active)),
+      user_id: user_id || create_user().id,
+      game_id: game_id || create_game().id,
+    })
+
+    # credo:disable-for-next-line
+    IO.inspect "Created Player-- #{player.id}"
+
+    player
+  end
+
+  defp create_game(user_id \\ nil) do
+    {:ok, game} = DB.Game.create(%{
+      status: FakerElixir.Helper.pick(~w(pending active complete)),
+      title: FakerElixir.Lorem.words,
+      owner_id: user_id || create_user().id,
+      round_length: FakerElixir.Number.between(5..50)
+    })
+
+    # credo:disable-for-next-line
+    IO.inspect "Created Game-- #{game.id}"
+
+    game
   end
 
   defp rand_string do
@@ -28,8 +81,8 @@ defmodule DB.Seed do
       System.argv
       |> List.first
 
-    String.to_integer(first_arg || "20")
+    String.to_integer(first_arg || "10")
   end
 end
 
-DB.Seed.create_users()
+DB.Seed.seed()
