b3471f422eb3baa6517b6b334973a5e8c40272f3
Cameron Irmas <cjirmas@gmail.com>
2017-10-09 20:10:55 -0700

    Update API tests

--- apps/api/test/job/update_game_test.exs
+++ apps/api/test/job/update_game_test.exs
@@ -1,7 +1,14 @@
-defmodule API.Job.ExecuteAtTest do
+defmodule API.Job.UpdateGameTest do
   use API.Web.ConnCase
 
-  test "update_game execute_at" do
+  test "updates the game round" do
+    game = API.Game.Factory.create_game()
+    API.Job.UpdateGame.run(game)
+    updated_game = DB.Game.get(game.id)
+    assert updated_game.round == game.round + 1
+  end
+
+  test "calculates the execute_at time" do
     start_time = %DateTime{
       calendar: Calendar.ISO, day: 20, hour: 18,
       microsecond: {273_806, 6}, minute: 58, month: 11, second: 19, std_offset: 0,
@@ -9,7 +16,7 @@ defmodule API.Job.ExecuteAtTest do
     }
 
     game = API.Game.Factory.create_game(%{round_length: 1, start_time: start_time, round: 1})
-    execute_at = API.Job.ExecuteAt.calculate("update_game", game)
+    execute_at = API.Job.UpdateGame.calculate(game)
     assert execute_at == 1_416_596_299
   end
 end
