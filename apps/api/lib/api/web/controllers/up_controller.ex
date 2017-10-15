d216a6e6cbec42120bc12644d5f6680218a48094
Michael Smykowski <smycal@gmail.com>
2017-10-15 12:26:45 -0700

    add redis to health check

--- apps/api/lib/api/web/controllers/up_controller.ex
+++ apps/api/lib/api/web/controllers/up_controller.ex
@@ -2,7 +2,10 @@ defmodule API.Web.UpController do
   use API.Web, :controller
 
   def up(conn, _params) do
-    checks = %{db: db_check()}
+    checks = %{
+      db: db_check(),
+      redis: redis_check()
+    }
 
     checks
     |> Map.values
@@ -21,6 +24,12 @@ defmodule API.Web.UpController do
   defp db_check do
     DB.User.get(1)
     :ok
+  rescue
+    _err -> :error
+  end
+
+  defp redis_check do
+    Verk.Stats.total()
   rescue
     _err -> :error
   end
